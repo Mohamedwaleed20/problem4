6a20aa1b36bfec671c6161a0f34545454934dde1
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-11 23:51:37 +0300

    Add project files.

---
+++ problem4/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("enter the num : ");
+int num =int .Parse(Console.ReadLine());
+for(int i = 1; i <= 12; i++)
+{
+   int res= num * i;
+    Console.WriteLine("{0}*{1}={2}",num,i,res);
+}
